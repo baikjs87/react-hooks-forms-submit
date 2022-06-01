b3e85d94376eaa137806fa60142dba81e7d21f61
Jason Baik <baikjs87@gmail.com>
2022-05-31 20:10:17 -0400

    Complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,64 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 function Form() {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+	const [firstName, setFirstName] = useState("Sylvia")
+	const [lastName, setLastName] = useState("Woods")
+	const [submittedData, setSubmittedData] = useState([])
+	const [errors, setErrors] = useState([])
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+	function handleFirstNameChange(event) {
+		setFirstName(event.target.value)
+	}
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+	function handleLastNameChange(event) {
+		setLastName(event.target.value)
+	}
 
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+	function handleSubmit(event) {
+		event.preventDefault()
+		if (firstName.length > 0) {
+			const formData = {
+				firstName: firstName,
+				lastName: lastName,
+			}
+			const dataArray = [...submittedData, formData]
+			setSubmittedData(dataArray)
+			setFirstName("")
+			setLastName("")
+			setErrors([])
+		} else {
+			setErrors(["First name is required!"])
+		}
+	}
+
+	const listOfSubmissions = submittedData.map((data, index) => {
+		return (
+			<ul>
+				<li key={index}>
+					{data.firstName} {data.lastName}
+				</li>
+			</ul>
+		)
+	})
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input type="text" onChange={handleFirstNameChange} value={firstName} placeholder={"First Name"} />
+				<input type="text" onChange={handleLastNameChange} value={lastName} placeholder={"Last Name"} />
+				<button type="submit">Submit</button>
+			</form>
+			{errors.length > 0
+				? errors.map((error, index) => (
+						<p key={index} style={{ color: "red" }}>
+							{error}
+						</p>
+				  ))
+				: null}
+			<h3>Submissions</h3>
+			{listOfSubmissions}
+		</div>
+	)
 }
 
-export default Form;
+export default Form
